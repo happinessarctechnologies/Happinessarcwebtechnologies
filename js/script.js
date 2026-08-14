8f4aea149afd76c0f9ed14a43963844ab3d281be
happinessarctechnologies <happinessarctechnologies@gmail.com>
2026-08-14 21:27:13 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
+// document.addEventListener("DOMContentLoaded", function () {
 
+//     const form = document.getElementById("contactForm");
+
+//     form.addEventListener("submit", function (event) {
+
+//         event.preventDefault();
+
+//         alert(
+//             "Thank you for contacting HappinessArc Web Technologies! We will get back to you soon."
+//         );
+
+//         form.reset();
+
+//     });
+
+// });
